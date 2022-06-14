58cf343e4f8f3d86326e075970b241f99fb574db
tchhabra-evolv <tchhabra@evolvtechnology.com>
2022-06-14 10:15:49 -0400

    this commit is just messing around

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,17 @@ import './App.css';
 
 function App() {
   return (
+
     <div className="App">
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+        <div className="shrink-0">
+          <img className="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo">
+        </div>
+        <div>
+          <div className="text-xl font-medium text-black">ChitChat</div>
+          <p className="text-slate-500">You have a new message!</p>
+        </div>
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
